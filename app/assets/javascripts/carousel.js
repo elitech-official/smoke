00d453275953ea503d7b9b28d8f808d45cca237b
elitech-official <completelydifferentuser@gmail.com>
2015-11-06 10:32:56 +0200

    menu

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
     closeEffect : 'none'
   });
   
+  $('.kalyan-item.container.cartridge').hide();
+  
   $('.list-group-item').click( function(event){ // лoвим клик пo ссылки с id="go"
     event.preventDefault(); // выключaем стaндaртную рoль элементa
     $('.list-group-item').removeClass('active');
     $(this).addClass('active');
     var cl = $(this).attr('data-id-cart');
-    alert('.'+ cl);
     $('.kalyan-item.container.cartridge').hide();
     $('.' + cl).css('display','block');
   });
